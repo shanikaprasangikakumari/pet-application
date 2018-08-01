0024ffa8990b078a6f826cc9973d6f188718e9d8
shanika <shanikak@hsenidmobile.com>
2018-08-01 08:46:32 +0530

    merage shanika_o1

--- module/application/src/main/java/pet/information/applications/SimpleApplication.java
+++ module/application/src/main/java/pet/information/applications/SimpleApplication.java
@@ -1,3 +1,16 @@
+
+/*
+ * Copyright (c) Copyright 2010-2018 hSenid Mobile Solutions (Pvt) Limited.
+ *               All Rights Reserved.
+ *
+ *               These materials are unpublished, proprietary, confidential source code of
+ *               hSenid Mobile Solutions (Pvt) Limited and constitute a TRADE SECRET
+ *               of hSenid Mobile Solutions (Pvt) Limited.
+ *
+ *               hSenid Mobile Solutions (Pvt) Limited retains all title to and intellectual
+ *               property rights in these materials.
+ */
+
 package pet.information.applications;
 
 import pet.information.applications.domain.Pet;
